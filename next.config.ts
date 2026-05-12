6078ae9103480573068b262cab93a6389fcf53d0
Devon Furuta <you@example.com>
2026-05-12 12:04:59 +0900

    Redirect app subdomain to dashboard

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "app.mekurureads.com",
+          },
+        ],
+        destination: "/dashboard",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
